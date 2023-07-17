bcc820e3ac2164563f6dacb81fa860d7ce194a60
Chickenboy71 <67282209+Chickenboy71@users.noreply.github.com>
2023-07-17 14:28:27 +0800

    clean up test.py and change name to gui.py

--- gui.py
+++ gui.py
@@ -15,7 +15,7 @@ import copy
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 testloader_custom = ds.test_dataloader_custom
 
-# on-demand models
+# load on-demand models (trained on 25, 50, 100, 150 epochs)
 ondemand_25 = copy.deepcopy(netG)
 checkpoint_25 = torch.load('checkpoints/checkpoint_ondemand_25.pth')
 ondemand_25.load_state_dict(checkpoint_25['model_state_dict'])
@@ -32,12 +32,13 @@ ondemand_150 = copy.deepcopy(netG)
 checkpoint_150 = torch.load('checkpoints/checkpoint_ondemand_final.pth')
 ondemand_150.load_state_dict(checkpoint_150['model_state_dict'])
 
+# set to evaluation mode as some layers behave differently during training and evaluation.
 ondemand_25.eval()
 ondemand_50.eval()
 ondemand_100.eval()
 ondemand_150.eval()
 
-# static models
+# load static models (trained on 25, 50, 100, 150 epochs)
 static_25 = copy.deepcopy(netG)
 checkpoint_25 = torch.load('checkpoints/checkpoint_static_25.pth')
 static_25.load_state_dict(checkpoint_25['model_state_dict'])
@@ -59,75 +60,11 @@ static_50.eval()
 static_100.eval()
 static_150.eval()
 
-# img_batch, hazy_batch = next(iter(testloader_custom))
-# with torch.inference_mode():
-#     output = netG(hazy_batch.to(device))
-#     # ds.calc_avg_psnr(ds.val_clean_dir, ds.val_hazy_dirs, netG, device)
-#     # ds.calc_avg_ssim(ds.val_clean_dir, ds.val_hazy_dirs, netG, device)
-
-# output = output.detach().cpu()
-
-# # visualisation
-# batch_size = hazy_batch.size(0)
-
-# # Create a grid of subplots with 1 row and 2 columns
-# fig, axes = plt.subplots(nrows=batch_size-5, ncols=2, figsize=(10, 10))
-
-# # Iterate over each image in the batch
-# for i in range(batch_size-5):
-#     # Convert the tensor images to numpy arrays
-#     hazy_image = hazy_batch[i].permute(1, 2, 0).cpu().numpy()
-#     output_image = output[i].permute(1, 2, 0).cpu().numpy()
-
-#     # Display hazy image in the left subplot
-#     axes[i, 0].imshow(hazy_image, aspect='auto')
-#     axes[i, 0].axis('off')
-
-#     # Display output image in the right subplot
-#     axes[i, 1].imshow(output_image, aspect='auto')
-#     axes[i, 1].axis('off')
-
-# # Adjust the spacing between subplots
-# plt.tight_layout()
-
-# # Show the plot
-# plt.show()
-#############################################################################
-# # test psnr calculation
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# checkpoint = torch.load('checkpoints/checkpoint_ondemand_final.pth')
-# netG.load_state_dict(checkpoint['model_state_dict'])
-# netG.eval()
-
-# val_clean_dir = "datasets/data/val/clean"
-# val_hazy_dirs = [
-#     "datasets/data/val/hazy/level1",
-#     "datasets/data/val/hazy/level2",
-#     "datasets/data/val/hazy/level3",
-#     "datasets/data/val/hazy/level4",
-#     "datasets/data/val/hazy/level5",
-# ]
-# train_hazy_dirs = [
-#     "datasets/data/train/hazy/level1",
-#     "datasets/data/train/hazy/level2",
-#     "datasets/data/train/hazy/level3",
-#     "datasets/data/train/hazy/level4",
-#     "datasets/data/train/hazy/level5",
-# ]
-# main_train_dir = "datasets/data/train/main"
-# start_time = time.time()
-# # psnr_scores = ds.calc_avg_psnr(val_clean_dir, val_hazy_dirs, netG, device)
-# ds.redistribute([5,5,5,5,5], train_hazy_dirs, main_train_dir)
-# if set(os.listdir("datasets/data/train/hazy/level1")) != set(os.listdir("datasets/data/train/main")):
-#     print("not equal")
-#     print( set(os.listdir("datasets/data/train/hazy/level1")) - set(os.listdir("datasets/data/train/main")))
-
-# print("--- %s seconds ---" % (time.time() - start_time))
-##############################################################################################
 # Gradio interface
 transform = transforms.Compose([transforms.ToTensor()])
 tensor_to_image = transforms.ToPILImage()    
 
+# Function to perform inference on all on-demand models
 def inference_ondemand(img):
     img = transform(img)
     img = img.unsqueeze(0)
@@ -142,7 +79,7 @@ def inference_ondemand(img):
         
     return outputs_ondemand
 
-
+# Function to perform inference on all static models
 def inference_static(img):
     img = transform(img)
     img = img.unsqueeze(0)
@@ -177,7 +114,6 @@ with gr.Blocks() as demo:
             output_ondemand_100 = gr.Image(height=256,width=256)
             output_ondemand_150 = gr.Image(height=256,width=256)
 
-
         button_ondemand.click(inference_ondemand, inputs = input_image, outputs = [output_ondemand_25, output_ondemand_50, output_ondemand_100, output_ondemand_150])
 
     with gr.Tab("Static Learning Model"):
@@ -198,7 +134,6 @@ with gr.Blocks() as demo:
                 output_static_50 = gr.Image(height=256,width=256)
                 output_static_100 = gr.Image(height=256,width=256)
                 output_static_150 = gr.Image(height=256,width=256)
-
 
             button_static.click(inference_static, inputs = input_image, outputs = [output_static_25, output_static_50, output_static_100, output_static_150])
 
